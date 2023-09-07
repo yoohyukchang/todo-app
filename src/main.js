f8a7cc891995a2dce25d32963cb377d413409640
yoohyukchang <yoohyukchang@gmail.com>
2023-09-07 00:14:31 -0400

    Initial commit: Set up project with Vite, TailwindCSS, and Prettier

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import "../style.css";
+
+const app = document.getElementById("app");
+app.innerHTML = `<div class="flex items-center justify-center h-screen">
+    <div>Hello World!</div>
+</div>`;
